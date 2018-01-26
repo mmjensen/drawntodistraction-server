5332068297c32c22f4281f1b28259d68467401f3
Mads Møller Jensen <mmjensen87@gmail.com>
2018-01-26 09:43:46 +0100

    Simple server saving user sessions.

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const errorhandler = require('errorhandler')
+const mongodb= require('mongodb')
+const logger = require('morgan')
+
+const url = 'mongodb://localhost:27017'
+
+const app = express();
+
+console.log("Server running")
+
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+mongodb.MongoClient.connect(url, (error, client) => {
+	if (error){
+		console.log("CONNECTION ERROR")
+		return process.exit(1)	
+	} 
+
+	var db = client.db('drawntodistraction')
+
+	app.get('/sessions/:id', (req, res) => {
+		if(!req.params.id){
+			console.log("No user ID provided!")
+			res.send("No user ID supplied")
+		} else {
+
+			var query = { userID: req.params.id };
+
+			db.collection('sessions')
+				.find(query)
+				.toArray((error, sessions) => {
+					if (error) return next(error)
+						res.send(sessions)
+			})
+		}
+	})
+
+	app.post('/sessions', (req,res) => {
+		let newSession = req.body
+		if(req.body.userID){
+			db.collection('sessions')
+			.insert(newSession, (error, results) => {
+				if (error) return next(error)
+				res.send(results)
+			})
+		} else {
+			res.send("Illegal POST request")
+		}
+	})
+
+  	app.listen(3000)
+})
